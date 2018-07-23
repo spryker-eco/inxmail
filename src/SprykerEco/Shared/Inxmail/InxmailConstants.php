887f3e3ecd99acda6026def8568fdf9baf1f4299
vhrychenko <aptah199494@gmail.com>
2018-07-23 15:31:38 +0300

    eco-1686 Add constants and config

---
+++ src/SprykerEco/Shared/Inxmail/InxmailConstants.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Shared\Inxmail;
+
+interface InxmailConstants
+{
+    public const KEY_ID = 'INXMAIL:KEY_ID';
+    public const SECRET = 'INXMAIL:SECRET';
+}
--- src/SprykerEco/Zed/Inxmail/InxmailConfig.php
+++ src/SprykerEco/Zed/Inxmail/InxmailConfig.php
@@ -8,7 +8,33 @@
 namespace SprykerEco\Zed\Inxmail;
 
 use Spryker\Zed\Kernel\AbstractBundleConfig;
+use SprykerEco\Shared\Inxmail\InxmailConstants;
 
 class InxmailConfig extends AbstractBundleConfig
 {
+    protected const INXMAIL_HEADERS = [];
+
+    /**
+     * @return string
+     */
+    public function getKeyId(): string
+    {
+        return $this->get(InxmailConstants::KEY_ID);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecret(): string
+    {
+        return $this->get(InxmailConstants::SECRET);
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return static::INXMAIL_HEADERS;
+    }
 }
