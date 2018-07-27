0d11697c163671a64c853086f7be72d750bfde4e
vhrychenko <aptah199494@gmail.com>
2018-07-27 14:00:31 +0300

    eco-1686 Customer restore password mail type

---
+++ src/SprykerEco/Zed/Inxmail/Communication/Plugin/Customer/InxmailCustomerRestorePasswordMailTypePlugin.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Zed\Inxmail\Communication\Plugin\Customer;
+
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface;
+use Spryker\Zed\Mail\Dependency\Plugin\MailTypePluginInterface;
+
+/**
+ * @method \SprykerEco\Zed\Inxmail\Business\InxmailFacadeInterface getFacade()
+ */
+class InxmailCustomerRestorePasswordMailTypePlugin extends AbstractPlugin implements MailTypePluginInterface
+{
+    const MAIL_TYPE = 'customer restore password';
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return static::MAIL_TYPE;
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return void
+     */
+    public function build(MailBuilderInterface $mailBuilder)
+    {
+        $this
+            ->setSubject($mailBuilder)
+            ->setHtmlTemplate($mailBuilder)
+            ->setTextTemplate($mailBuilder)
+            ->setRecipient($mailBuilder)
+            ->setSender($mailBuilder);
+
+        $this->getFacade()->handleCustomerResetPasswordEvent($mailBuilder->getMailTransfer()->getCustomer());
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return $this
+     */
+    protected function setSubject(MailBuilderInterface $mailBuilder)
+    {
+        $mailBuilder->setSubject('mail.customer.restore_password.subject');
+
+        return $this;
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return $this
+     */
+    protected function setHtmlTemplate(MailBuilderInterface $mailBuilder)
+    {
+        $mailBuilder->setHtmlTemplate('customer/mail/customer_restore_password.html.twig');
+
+        return $this;
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return $this
+     */
+    protected function setTextTemplate(MailBuilderInterface $mailBuilder)
+    {
+        $mailBuilder->setTextTemplate('customer/mail/customer_restore_password.text.twig');
+
+        return $this;
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return $this
+     */
+    protected function setRecipient(MailBuilderInterface $mailBuilder)
+    {
+        $customerTransfer = $mailBuilder->getMailTransfer()->requireCustomer()->getCustomer();
+
+        $mailBuilder->addRecipient(
+            $customerTransfer->getEmail(),
+            $customerTransfer->getFirstName() . ' ' . $customerTransfer->getLastName()
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param \Spryker\Zed\Mail\Business\Model\Mail\Builder\MailBuilderInterface $mailBuilder
+     *
+     * @return $this
+     */
+    protected function setSender(MailBuilderInterface $mailBuilder)
+    {
+        $mailBuilder->setSender('mail.sender.email', 'mail.sender.name');
+
+        return $this;
+    }
+}
