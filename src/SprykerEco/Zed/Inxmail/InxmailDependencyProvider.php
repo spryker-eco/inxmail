e2b87264f1e2495fea7847a6bcdb5837560a1e26
vhrychenko <aptah199494@gmail.com>
2018-08-28 17:29:08 +0300

    ECO-1686 Update composer

--- src/SprykerEco/Zed/Inxmail/InxmailDependencyProvider.php
+++ src/SprykerEco/Zed/Inxmail/InxmailDependencyProvider.php
@@ -4,6 +4,7 @@
  * MIT License
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
+
 namespace SprykerEco\Zed\Inxmail;
 
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
